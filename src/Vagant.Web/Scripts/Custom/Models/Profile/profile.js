446cf11f8dbb29b05efb6ecf2695db5b38a20b0d
Anton Pashkouski <Wolendrang@gmail.com>
2016-06-18 15:34:33 +0300

    Template of profile page was added.

---
+++ src/Vagant.Web/Scripts/Custom/Models/Profile/profile.js
@@ -0,0 +1,17 @@
+﻿var Profile = Profile || {};
+Profile.constructors = Profile.constructors || {};
+
+Profile.constructors.Profile = (function ($, ko) {
+
+    function ProfilePage() {
+        var self = this;
+
+        self.contactInformation = new Profile.constructors.ContactInformation();
+        self.achievements = new Profile.constructors.Achievements();
+        self.profileHistory = new Profile.constructors.ProfileHistory();
+    }
+
+    ko.applyBindings(new ProfilePage());
+    return ProfilePage();
+
+})(jQuery, ko);

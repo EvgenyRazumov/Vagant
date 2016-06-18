533c9551211e3f7085074f82b9e95d4d6f2ded4e
Evgeny <razumovhome@gmail.com>
2016-06-18 16:08:46 +0300

    full schema.

--- src/Vagant.Data/ApplicationDbContext.cs
+++ src/Vagant.Data/ApplicationDbContext.cs
@@ -13,5 +13,32 @@ namespace Vagant.Data
         public DbSet<ImageFile> ImageFiles { get; set; }
 
         public DbSet<UserContactInfo> UserContactInfos { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<EventComment> EventComments { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<EventImage> EventImages { get; set; }
+
+        public DbSet<Visitor> Visitors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<EventImage>()
+            .HasRequired(c => c.ImageFile)
+            .WithMany()
+            .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<EventImage>()
+                .HasRequired(s => s.Event)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+        }
     }
 }
--- src/Vagant.Domain/Entities/UserContactInfo.cs
+++ src/Vagant.Domain/Entities/UserContactInfo.cs
@@ -3,9 +3,13 @@
     public class UserContactInfo : BaseEntity
     {
         public string SkypeName { get; set; }
+
         public string VkProfileName { get; set; }
+
         public string FacebookProfileName { get; set; }
+
         public string TwitterProfileName { get; set; }
+
         public string InstagramProfileName { get; set; }
     }
 }

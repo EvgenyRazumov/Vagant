489558782b76b126e4537794de2f33da253c6f18
Igor Tatarnikov <igor.tatarnikov@itechart-group.com>
2016-06-18 14:20:18 +0300

    ProfileController was added

--- src/Vagant.Web/Controllers/ProfileController.cs
+++ src/Vagant.Web/Controllers/ProfileController.cs
@@ -1,13 +1,68 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using Vagant.Web.Models.Profile;
 
 namespace Vagant.Web.Controllers
 {
-    public class ProfileController : Controller
+    public class ProfileController: BaseController
     {
-        // GET: Profile
-        public ActionResult Index()
+        #region Ctor
+
+        public ProfileController()
         {
-            return View();
+
         }
+
+        #endregion
+
+        #region Actions
+
+        public ActionResult Index(int userId)
+        {
+            try
+            {
+                var viewModel = GetEditableProfileViewModel(userId);
+                return View(viewModel);
+            }
+            catch (Exception)
+            {
+                //todo: log error
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Details(int userId)
+        {
+            try
+            {
+                var viewModel = GetProfileDetailsViewModel(userId);
+                return View(viewModel);
+            }
+            catch (Exception)
+            {
+                //todo: log error
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private EditProfileViewModel GetEditableProfileViewModel(int userId)
+        {
+            var result = new EditProfileViewModel();
+
+            return result;
+        }
+
+        private ProfileDetailsViewModel GetProfileDetailsViewModel(int userId)
+        {
+            var result = new ProfileDetailsViewModel();
+
+            return result;
+        }
+
+        #endregion
     }
 }
